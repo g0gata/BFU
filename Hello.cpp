156049c06fa8e8f4b2dc7804e37e3f081c6bf867
g0gata <g.kostov90@abv.bg>
2018-03-12 16:39:29 +0200

    update

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // cout , cin
+#include<cstdlib> // system
+using namespace std;
+int main()
+{
+     system ("chcp 1251");
+    cout << "«драсти е ман€чи!!!!" << endl;
+    system ("pause");
+    return 0;
+}
